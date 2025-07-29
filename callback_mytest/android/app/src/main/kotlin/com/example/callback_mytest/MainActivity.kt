d468b97827349353e29d1696c9cf482c1a7d3a6c
sonjiyoon12 <1wldbs1908@gmail.com>
2025-07-29 09:00:06 +0900

    변경사항

--- callback_mytest/android/app/src/main/kotlin/com/example/callback_mytest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.callback_mytest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- class_profile_mytest/android/app/src/main/kotlin/com/example/class_profile_mytest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.class_profile_mytest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
