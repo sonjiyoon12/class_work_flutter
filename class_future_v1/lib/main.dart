ff207d283a783b2a8b4cb0f2ec70131bc4b67e12
sonjiyoon12 <1wldbs1908@gmail.com>
2025-07-29 13:59:38 +0900

    비동기 프로그래밍 future

--- class_future_v1/lib/main.dart
+++ class_future_v1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   // dart 비동기 프로그래밍 개념잡기
   // 동기성 : 모든 코드가 순차적으로 진행되는 형태
   // 비동기 : 코드가 순차적으로 진행을 보장할 수 없는 형태
@@ -9,20 +9,18 @@ void main() {
 
   print('task 1 ... ');
 
-  // 콜백 개념으로 동작 한다
-  var data1 = fetchData().then(
-    (value) {
-      print("결과값 : ${value}");
-      return value;
-    },
-  );
+  // 1. 비동기 함수를 필요에 의해서 동기적으로 변경하는 방법
+  // await 키워드
+  var data1 = await fetchData();
+  // 비동기적 방식인데 필요하다면 동기적 방식으로 처리해야 하는 구나!!
   print('task 2 ... ${data1}');
+
   print('task 3 ...');
 }
 
 Future<String> fetchData() {
   return Future.delayed(Duration(seconds: 5), () {
     print('fetchData() 메서드 호출');
-    return "5초 Data";
+    return "2초 Data";
   });
 }
