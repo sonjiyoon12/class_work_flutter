1a7d6d9618d94ec40eeb485bd12b927d0ea4b72c
sonjiyoon12 <1wldbs1908@gmail.com>
2025-07-31 14:12:49 +0900

    에어비엔비웹만들기

---
+++ class_airbnb/lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+// 색상
+const kAccentColor = Color(0xFFFF385C);
---
+++ class_airbnb/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:class_airbnb/components/home/home_body.dart';
+import 'package:class_airbnb/components/home/home_header.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          HomeHeader(),
+          HomeBody(),
+        ],
+      ),
+    );
+  }
+}
